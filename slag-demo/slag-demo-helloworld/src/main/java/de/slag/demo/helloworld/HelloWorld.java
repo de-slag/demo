c6a22e5009838e231870e40ac1c675ceb01ad3dc
slipp <slipp@NB308.coinor.local>
2020-05-28 08:34:06 +0200

    hello world in separate mvn project

---
+++ slag-demo/slag-demo-helloworld/src/main/java/de/slag/demo/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package de.slag.demo.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
--- slag-demo/slag-demo-jsf-webapp/src/main/java/de/slag/demo/HelloWorldController.java
+++ slag-demo/slag-demo-jsf-webapp/src/main/java/de/slag/demo/HelloWorldController.java
@@ -1,14 +1,33 @@
 package de.slag.demo;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
+import de.slag.demo.helloworld.HelloWorld;
+
 
 @ManagedBean
 @SessionScoped
 public class HelloWorldController {
-	
+
 	public String getHello() {
-		return "Hello World!";
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		final PrintStream ps = new PrintStream(baos);
+		try {
+			return getHello0(baos, ps);
+		} catch (Throwable t) {
+			System.setOut(System.out);
+			throw t;
+		}
+	}
+
+	private String getHello0(ByteArrayOutputStream baos, PrintStream ps) {
+		System.setOut(ps);
+		HelloWorld.main(new String[0]);
+		return baos.toString();
 	}
 
 }
