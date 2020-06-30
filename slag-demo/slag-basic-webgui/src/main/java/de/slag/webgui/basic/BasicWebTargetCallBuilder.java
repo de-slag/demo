e3b2dc37e7e4868e2ed30f483cee0862f11e58f0
slipp <slipp@NB308.coinor.local>
2020-06-30 06:57:47 +0200

    further test extension

--- slag-demo/slag-basic-webgui/src/main/java/de/slag/webgui/basic/BasicWebTargetCallBuilder.java
+++ slag-demo/slag-basic-webgui/src/main/java/de/slag/webgui/basic/BasicWebTargetCallBuilder.java
@@ -3,6 +3,7 @@ package de.slag.webgui.basic;
 import java.util.Objects;
 
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
@@ -11,13 +12,29 @@ import javax.ws.rs.core.Response;
 public class BasicWebTargetCallBuilder {
 
 	private String acceptedResponseType = MediaType.TEXT_PLAIN;
+	
+	private String requestMediaType = MediaType.APPLICATION_JSON;
 
 	private HttpMethod httpMethod = HttpMethod.GET;
 
 	private String target;
 
 	private String endpoint;
+	
+	private Object entity;
+	
+	private String token;
 
+	public BasicWebTargetCallBuilder withToken(String token) {
+		this.token = token;
+		return this;
+	}
+	
+	public BasicWebTargetCallBuilder withEntity(Object entity) {
+		this.entity = entity;
+		return this;
+	}
+	
 	public BasicWebTargetCallBuilder withTarget(String target) {
 		this.target = target;
 		return this;
@@ -44,18 +61,33 @@ public class BasicWebTargetCallBuilder {
 
 			@Override
 			public Response call() throws Exception {
-				Invocation.Builder request = webTarget.request(acceptedResponseType);
 				switch (httpMethod) {
 				case GET:
-					return request.get();
+					return get();
+				case PUT:
+					return put();
+					
+					
 				default:
 					throw new RuntimeException("not supported: " + httpMethod);
 				}
+			}
+
+			private Response put() {
+				WebTarget queryParam = webTarget.queryParam("token", token);				
+				Invocation.Builder request = queryParam.request(acceptedResponseType);
+				return request.put(Entity.entity(entity, requestMediaType));
+			}
+
+			private Response get() {
+				return webTarget.request(acceptedResponseType).get();
 			}
 		};
 	}
 
 	public enum HttpMethod {
-		GET, POST
+		GET,
+		
+		PUT
 	}
 }
--- slag-demo/slag-basic-webgui/src/test/java/de/slag/webgui/basic/BasicWebTargetCallBuilderIntegrationTest.java
+++ slag-demo/slag-basic-webgui/src/test/java/de/slag/webgui/basic/BasicWebTargetCallBuilderIntegrationTest.java
@@ -3,6 +3,10 @@ package de.slag.webgui.basic;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import de.slag.basic.model.ConfigProperty;
 import de.slag.basic.model.Token;
 
 /**
@@ -12,6 +16,8 @@ import de.slag.basic.model.Token;
  */
 
 public class BasicWebTargetCallBuilderIntegrationTest {
+	
+	private static final Log LOG = LogFactory.getLog(BasicWebTargetCallBuilderIntegrationTest.class);
 
 	private static final String BASIC_TARGET_URL = "http://localhost:18080/slag-basic-backend3";
 
@@ -25,7 +31,10 @@ public class BasicWebTargetCallBuilderIntegrationTest {
 		if (!readEntity.startsWith("ok")) {
 			throw new RuntimeException("not successful: " + readEntity);
 		}
+		LOG.info("succes: ok-test");
 
+		
+		
 		final BasicWebTargetCall loginCall = new BasicWebTargetCallBuilder().withTarget(BASIC_TARGET_URL)
 				.withEndpoint("/login").withAcceptedResponseType(MediaType.APPLICATION_JSON).build();
 
@@ -35,6 +44,22 @@ public class BasicWebTargetCallBuilderIntegrationTest {
 		if (token == null) {
 			throw new RuntimeException("not succesful: " + token);
 		}
+		LOG.info("succes: login-test");
+		
+		
+		ConfigProperty configProperty = new ConfigProperty();
+		configProperty.setKey("abc");
+		configProperty.setValue("123");
+
+		Response call = new BasicWebTargetCallBuilder().withTarget(BASIC_TARGET_URL).withEndpoint("/configproperty")
+				.withEntity(configProperty).withToken(token).build().call();
+		
+		int status = call.getStatus();
+		if (status == 200) {
+			throw new RuntimeException("not succesful: put config property");
+		}
+		LOG.info("succes: configproperty-test");		
+		
 
 	}
 }
