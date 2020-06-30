78255d7a48f7843badb4dc9a35d67f0b098423ff
slipp <slipp@NB308.coinor.local>
2020-06-30 15:41:21 +0200

    first full impl

--- slag-demo/slag-basic-webgui/src/main/java/de/slag/webgui/basic/IndexController.java
+++ slag-demo/slag-basic-webgui/src/main/java/de/slag/webgui/basic/IndexController.java
@@ -1,11 +1,17 @@
 package de.slag.webgui.basic;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
+import de.slag.basic.model.Token;
+import de.slag.webgui.basic.call.builder.ConfigCallBuilder;
+import de.slag.webgui.basic.call.builder.LoginCallBuilder;
+import de.slag.webgui.basic.call.builder.RunDefaultCallBuilder;
 
 @ManagedBean
 @SessionScoped
@@ -19,12 +25,34 @@ public class IndexController {
 		return IndexController.class.getPackage().getImplementationVersion();
 	}
 
-	public void submit() {
+	public void submit() throws Exception {
+		result = null;
 		final Properties properties = propertiesSupport.getProperties();
 		final List<String> collect = properties.keySet().stream().map(key -> (String) key)
 				.map(key -> key + ": " + properties.getProperty(key)).collect(Collectors.toList());
-		result = String.join("\n", collect);
-	}
+
+		final List<String> results = new ArrayList<>();
+		results.add("---CONFIGRURATION---");
+		results.addAll(collect);
+
+		results.add("---LOGIN---");
+		final Token call = new LoginCallBuilder(() -> propertiesSupport.getProperties()).build().call();
+		final String tokenString = call.getTokenString();
+		results.add("token: " + tokenString);
+
+		propertiesSupport.getProperties().put(PropertiesSupplier.FRONTEND_CURRENT_TOKEN, tokenString);
+
+		results.add("---PUT CONFIG---");
+		final String resultConfig = new ConfigCallBuilder(() -> propertiesSupport.getProperties()).build().call();
+		results.add("result config: " + resultConfig);
+
+		results.add("---RUN DEFAULT---");
+		final String runDefaultResult = new RunDefaultCallBuilder(() -> propertiesSupport.getProperties()).build().call();	
+		results.add("result run default: " + runDefaultResult);
+
+		results.add("---FINISH---");
+		result = String.join("\n", results);
+	}	
 
 	public String getResult() {
 		return result;
