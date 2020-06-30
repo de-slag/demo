6f93fcf3fbccc34e2eed6b2757ffabda455e4b8b
slipp <slipp@NB308.coinor.local>
2020-06-30 15:12:38 +0200

    extend init web gui frontend pages
    index
    config

--- slag-demo/slag-basic-webgui/src/main/java/de/slag/webgui/basic/ConfigController.java
+++ slag-demo/slag-basic-webgui/src/main/java/de/slag/webgui/basic/ConfigController.java
@@ -1,11 +1,18 @@
 package de.slag.webgui.basic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
+import org.apache.commons.lang.StringUtils;
 
 @ManagedBean
 @SessionScoped
 public class ConfigController {
+
+	private PropertiesSupport propertiesSupport = PropertiesSupport.getInstance();
 
 	private String configText;
 
@@ -20,13 +27,23 @@ public class ConfigController {
 	}
 
 	public void save() {
+		propertiesSupport.getProperties().clear();
+		if (StringUtils.isEmpty(configText)) {
+			return;
+		}
+		final List<String> asList = Arrays.asList(configText.split("\n"));
+		asList.forEach(keyValue -> {
+			final String[] split = keyValue.split("=");
+			final String key = split[0];
+			final String value = split[1];
+			propertiesSupport.getProperties().put(key, value);
+		});
+
 		status = "saved";
 	}
 
 	public String getStatus() {
 		return status;
 	}
-	
-	
 
 }
--- slag-demo/slag-basic-webgui/src/main/java/de/slag/webgui/basic/IndexController.java
+++ slag-demo/slag-basic-webgui/src/main/java/de/slag/webgui/basic/IndexController.java
@@ -1,15 +1,33 @@
 package de.slag.webgui.basic;
 
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 @ManagedBean
 @SessionScoped
 public class IndexController {
-	
+
+	private PropertiesSupport propertiesSupport = PropertiesSupport.getInstance();
+
+	private String result;
+
 	public String getVersion() {
-		return IndexController.class.getPackage()
-				.getImplementationVersion();
+		return IndexController.class.getPackage().getImplementationVersion();
+	}
+
+	public void submit() {
+		final Properties properties = propertiesSupport.getProperties();
+		final List<String> collect = properties.keySet().stream().map(key -> (String) key)
+				.map(key -> key + ": " + properties.getProperty(key)).collect(Collectors.toList());
+		result = String.join("\n", collect);
+	}
+
+	public String getResult() {
+		return result;
 	}
 
 }
