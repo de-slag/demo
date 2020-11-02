5c628dcb4b8b465b8dc2d9bd3e0203ac79d0ab6f
Sebastian Lipp <post@sebastianlipp.de>
2020-11-02 20:32:10 +0100

    test

--- slag-demo/invest-alphavantage4j/src/test/java/de/slag/invest/a4j/InvestA4JRunnerTest.java
+++ slag-demo/invest-alphavantage4j/src/test/java/de/slag/invest/a4j/InvestA4JRunnerTest.java
@@ -1,18 +1,25 @@
 package de.slag.invest.a4j;
 
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 class InvestA4JRunnerTest {
+	
+	private static final String API_KEY = "SEOG69AIA6X9PGR2";
 
+	private static final String SYMBOL_WKN_ISIN_MAPPING = "MSFT:US5949181045;IBM:US4592001014";
+	
+	private static final List<String> SYMBOLS = Arrays.asList("MSFT","IBM");
+	
 	@Test
 	void integrationTest() {
 		InvestA4JRunner investA4JRunner = new InvestA4JRunnerBuilder()
-			.withApiKey("50M3AP1K3Y")
-			.withSymbols(Collections.singletonList("MSFT"))
+			.withApiKey(API_KEY)
+			.withSymbols(SYMBOLS)
 			.withOutputFolder(System.getProperty("java.io.tmpdir"))
-			.withSymbolWknIsinMapping("MSFT=846901;DAX=846900")
+			.withSymbolWknIsinMapping(SYMBOL_WKN_ISIN_MAPPING)
 			.build();
 		investA4JRunner.run();
 	}
